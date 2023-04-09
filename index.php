725d1537bc1088e57e81e0c14821bb5fe4de3329
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-04-09 10:38:35 +0200

    Ajout HTML de la première section

--- index.php
+++ index.php
@@ -2,11 +2,55 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="style.css">
         <title>Kukra</title>
     </head>
     <body>
-        
+        <section id="accueil">
+            <header id="barreNavigation">
+                <h1>Kucra</h1>
+                <nav>
+                    <ul class="listeNavigation">
+                        <li>HOME</li>
+                        <li>FEATURES</li>
+                        <li>CLIENT</li>
+                        <li>PRICING</li>
+                        <li>FAQ</li>
+                        <li>ABOUT</li>
+                        <li>BLOG</li>
+                        <li>CONTACT</li>
+                    </ul>
+                </nav>
+                <nav>
+                    <ul class="listeNavigation">
+                        <li><i class="fa fa-facebook" aria-hidden="true"></i></li>
+                        <li><i class="fa fa-twitter" aria-hidden="true"></i></li>
+                        <li><i class="fa fa-instagram" aria-hidden="true"></i></li>
+                    </ul>
+                </nav>
+            </header>
+            <article id="articleAccueil">
+                <article>
+                    <h2>We are StartUp Creative Kucra Agency</h2>
+                    <p>
+                        Carefully crafted after analysing the needs of 
+                        different industries and the design achieves a 
+                        great balance between purpose & presentation
+                    </p>
+                    <form action="">
+                        <input type="text" name="email" id="email" placeholder="Enter your email">
+                        <input type="submit" value="SUBSCRIBE">
+                    </form>
+                </article>
+                <aside>
+                    <figure>
+                        <img src="img/illustration.svg" alt="Écran d'ordinateur illustratif avec le contenu de celui-ci en 3 dimensions et un chat par dessus" id="imageOrdinateur">
+                    </figure>
+                </aside>
+            </article>
+        </section>
     <script src="script.js"></script>
     </body>
 </html>
