73c163a23efd08007ae88471b83fad343299a85e
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-04-09 09:42:19 +0200

    Ajout des fichiers de base

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-    echo "Test";
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <title>Kukra</title>
+    </head>
+    <body>
+        
+    <script src="script.js"></script>
+    </body>
+</html>
