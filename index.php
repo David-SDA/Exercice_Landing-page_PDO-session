9887bc72bd96c5ca92978617eceec904c0f0eaa9
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-04-10 18:47:56 +0200

    Ajustements, html de la deuxième section

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -17,7 +17,7 @@
                 <nav>
                     <ul class="listeNavigation">
                         <li><a href="#accueil">HOME</a></li>
-                        <li><a href="#">FEATURES</a></li>
+                        <li><a href="#features">FEATURES</a></li>
                         <li><a href="#">CLIENT</a></li>
                         <li><a href="#">PRICING</a></li>
                         <li><a href="#">FAQ</a></li>
@@ -28,9 +28,9 @@
                 </nav>
                 <nav>
                     <ul class="listeNavigation">
-                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
                     </ul>
                 </nav>
             </header>
@@ -52,6 +52,37 @@
                         <img src="img/illustration.svg" alt="Écran d'ordinateur illustratif avec le contenu de celui-ci en 3 dimensions et un chat par dessus" id="imageOrdinateur">
                     </figure>
                 </aside>
+            </article>
+        </section>
+        <section id="features">
+            <header>
+                <h1>Products Features</h1>
+                <p>
+                    It is a long established fact that a reader will be of a page when established fact looking at its layout
+                </p>
+            </header>
+            <article id="cartes">
+                <article class="carte">
+                    <h2><i class="fa-solid fa-globe"></i></h2>
+                    <h3>Digital Design</h3>
+                    <p>
+                        Some quick example text to build on the card title and make up the bulk of the card platform.
+                    </p>
+                </article>
+                <article class="carte">
+                    <h2><i class="fa-sharp fa-solid fa-brush"></i></h2>
+                    <h3>Unlimited Colors</h3>
+                    <p>
+                        Credibly brand stanaads compliant user exteible services College Anibh ocean euismod tincidunt laoreet.
+                    </p>
+                </article>
+                <article class="carte">
+                    <h2><i class="fa-solid fa-chess"></i></h2>
+                    <h3>Strategy Solutions</h3>
+                    <p>
+                        Separated they live in Bookmarks grove right at the coast of the Semantics, a large ocean regelialia.
+                    </p>
+                </article>
             </article>
         </section>
     <script src="script.js"></script>
